0788ad139963ed9f9e98246de6f53767c06d31a3
CRYSTALYN GAGUIS <crystalyn.gaguis@ibm.com>
2019-07-22 15:14:17 +0800

    hellochanges

--- src/main/java/com/ibm/cams/gh/bankonline/cams_gh_bankonline/controller/EnrolledMerchantController.java
+++ src/main/java/com/ibm/cams/gh/bankonline/cams_gh_bankonline/controller/EnrolledMerchantController.java
@@ -3,6 +3,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.ibm.cams.gh.bankonline.cams_gh_bankonline.domain.EnrolledMerchant;
+import com.ibm.cams.gh.bankonline.cams_gh_bankonline.domain.EnrolledMerchantUI;
 import com.ibm.cams.gh.bankonline.cams_gh_bankonline.repository.EnrolledMerchantRepo;
 
 @RestController
@@ -20,6 +21,13 @@ public class EnrolledMerchantController {
 		return merchants;
 	}
 	
-	
+	@PostMapping("/addEnMerchant")
+	public String create(@RequestBody EnrolledMerchantUI enrolledMerchant){
+		// save a single Customer
+		EnrolledmerchRepo.save(new EnrolledMerchant(enrolledMerchant.getEnmerch_id(),
+							   enrolledMerchant.getMerch_id(),enrolledMerchant.getAccountnumber()));
+
+		return "Transaction is created";
+	}
 	
 }
--- src/main/java/com/ibm/cams/gh/bankonline/cams_gh_bankonline/controller/TransactionController.java
+++ src/main/java/com/ibm/cams/gh/bankonline/cams_gh_bankonline/controller/TransactionController.java
@@ -3,7 +3,9 @@ package com.ibm.cams.gh.bankonline.cams_gh_bankonline.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import com.ibm.cams.gh.bankonline.cams_gh_bankonline.domain.Transaction;
+import com.ibm.cams.gh.bankonline.cams_gh_bankonline.domain.TransactionUI;
 import com.ibm.cams.gh.bankonline.cams_gh_bankonline.repository.TransactionRepo;
 
 @RestController
@@ -18,5 +20,14 @@ public class TransactionController {
 		List<Transaction> transaction = transactionRepo.findAll();
 
 		return transaction;
+	}
+	
+	@PostMapping("/addTransaction")
+	public String create(@RequestBody TransactionUI transaction){
+		// save a single Customer
+		transactionRepo.save(new Transaction(transaction.getTrans_id(), transaction.getMerch_id(), 
+							transaction.getAccountnumber(), transaction.getAmount(),transaction.getTrans_date()));
+
+		return "Transaction is created";
 	}
 }
